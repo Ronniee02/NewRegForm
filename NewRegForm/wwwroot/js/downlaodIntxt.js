0f3407e63ec7f69e0c7b04a9b8021d6a30162ea4
Ranjit Kumar Sharma <ranjit.sharma@csharptek.com>
2023-01-10 10:54:32 +0530

    Add project files.

---
+++ NewRegForm/wwwroot/js/downlaodIntxt.js
@@ -0,0 +1,30 @@
+﻿
+    function save() {
+    var text = document.getElementById("fname").value;
+    var text2 = document.getElementById("lname").value;
+    var text3 = document.getElementById("email").value;
+    var text4 = document.getElementById("mobile").value;
+    var text5 = document.getElementById("dob").value;
+    var text6 = document.getElementById("address").value;
+    var text7 = document.getElementById("city").value;
+    var text8 = document.getElementById("pin").value;
+    var text9 = document.getElementById("state").value;
+    var text11 = document.getElementById("qualification").value;
+    var text12 = document.getElementById("c").value;
+    var text13 = document.getElementById("password").value;
+
+    all = (text + "\n" + text2 + "\n" + text3 + "\n" + text4 + "\n" + text5 + "\n" + text6 + "\n" + text7 + "\n" + text8 + "\n" + text9 + "\n"  + "\n" + text11 + "\n" + text12 + "\n" + text13).replace(/\n/g, "\r\n");
+    /*text2 = text2.replace(/\n/g, "\r\n");*/
+
+
+    var blob = new Blob([all], {type: "text/plain" });
+    var anchor = document.createElement("a");
+    anchor.download = "keyboard_lexilogos.txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target = "_blank";
+    anchor.style.display = "none";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+
+        }
---
+++ NewRegForm/wwwroot/js/filled_or_Not.js
@@ -0,0 +1,83 @@
+﻿<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+function check() {
+    var name = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("mobile").value;
+    var gender = document.getElementById("g").value;
+    var dob = document.getElementById("dob").value;
+    var address = document.getElementById("address").value;
+    var city = document.getElementById("city").value;
+    var pincode = document.getElementById("pin").value;
+    var state = document.getElementById("state").value;
+    var qualification = document.getElementById("qualification").value;
+   /* var specialization = document.getElementById("c").value;*/
+    var password = document.getElementById("password").value;
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;  //Javascript reGex for Email Validation.
+    var regPhone = /^\d{10}$/;                                        // Javascript reGex for Phone Number validation.
+    var regName = /\d+$/g;                                    // Javascript reGex for Name validation
+
+
+    if (name == '' || lname == '' || email == '' || phone == '' || gender == '' || dob == '' || address == '' || city == '' || pincode == '' || state == '' || qualification == '' || /*specialization == '' ||*/ password == '') {
+        swal({
+            title: "Fields Empty",
+            text: "Please Check the missing field!!",
+            icon: "Warning",
+            button: "ok",
+        });
+    } else {
+
+        swal({
+            title: "Succesfully Submitted",
+           
+            icon: "success",
+            button: "yes",
+           
+        });
+
+    }
+    if (name == "" || regName.test(name)) {
+        window.alert("Please enter your name properly.");
+        name.focus();
+        return false;
+    }
+
+    if (address == "") {
+        window.alert("Please enter your address.");
+        address.focus();
+        return false;
+    }
+
+    if (email == "" || !regEmail.test(email)) {
+        window.alert("Please enter a valid e-mail address.");
+        email.focus();
+        return false;
+    }
+
+    if (password == "") {
+        alert("Please enter your password");
+        password.focus();
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password should be atleast 6 character long");
+        password.focus();
+        return false;
+
+    }
+    if (phone == "" || !regPhone.test(phone)) {
+        alert("Please enter valid phone number.");
+        phone.focus();
+        return false;
+    }
+
+    //if (what.selectedIndex == -1) {
+    //    alert("Please enter your course.");
+    //    what.focus();
+    //    return false;
+    //}
+
+    return true;
+}
+        
